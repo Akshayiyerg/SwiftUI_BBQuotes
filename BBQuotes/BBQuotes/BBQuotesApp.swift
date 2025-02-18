bfb6c3d9e7653dbdd1b3a3a7499ac9ed5c556f52
Akshay Ganesh <akshayiyerg@gmail.com>
2025-02-17 20:19:13 -0500

    Initail Commit for the project. Assests added and also few sample data.

---
+++ BBQuotes/BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Akshay  on 2025-02-17.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
