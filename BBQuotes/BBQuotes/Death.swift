fccd047dce4580202d2a3e702662022d8193726e
Akshay Ganesh <akshayiyerg@gmail.com>
2025-02-18 23:25:09 -0500

    Model for Death data added.

---
+++ BBQuotes/BBQuotes/Death.swift
@@ -0,0 +1,15 @@
+//
+//  Death.swift
+//  BBQuotes
+//
+//  Created by Akshay  on 2025-02-18.
+//
+
+import Foundation
+
+struct Death: Decodable {
+    let character: String
+    let image: URL
+    let detail: String
+    let lastWords: String
+}
