217310ba768a0e199280d5247d8f615d96072f23
Akshay Ganesh <akshayiyerg@gmail.com>
2025-02-18 23:22:52 -0500

    Model added for Character type.

---
+++ BBQuotes/BBQuotes/Char.swift
@@ -0,0 +1,18 @@
+//
+//  Char.swift
+//  BBQuotes
+//
+//  Created by Akshay  on 2025-02-18.
+//
+
+import Foundation
+
+struct char: Decodable {
+    let name: String
+    let birthday: String
+    let occupations: [String]
+    let images: [URL]
+    let aliases: [String]
+    let status: String
+    let portrayedBy: String
+}
