6c9ca950729b9821ee3c1c99001a3e1493681b15
Akshay Ganesh <akshayiyerg@gmail.com>
2025-02-23 23:17:41 -0500

    Building fetch function.

--- BBQuotes/BBQuotes/FetchService.swift
+++ BBQuotes/BBQuotes/FetchService.swift
@@ -16,17 +16,21 @@ struct FetchService {
     let baseURL = URL(string: "https://breaking-bad-api-six.vercel.app/api")!
     
     // https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
-    func fetchQuotes(from show: String) {
+    func fetchQuotes(from show: String) async throws -> Quote {
         // Build fetch url
         let quoteURL = baseURL.appending(path: "quotes/random")
         let fetchURL = quoteURL.appending(queryItems: [URLQueryItem(name: "production", value: show)])
             
         // Fetch data
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
         
         // Handle response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw FetchError.badResponse }
         
         // Decode data
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
         
         // Return quote
+        return quote
     }
 }
