ac02ff031547fd4acc9fdf25f2760f4119944a2b
Akshay Ganesh <akshayiyerg@gmail.com>
2025-02-27 21:06:16 -0500

    BackgroundImage and Quote text added.

--- BBQuotes/BBQuotes/QuoteView.swift
+++ BBQuotes/BBQuotes/QuoteView.swift
@@ -15,8 +15,23 @@ struct QuoteView: View {
         GeometryReader { geo in
             ZStack {
                 Image(show.lowercased().replacingOccurrences(of: " ", with: ""))
+                    .resizable()
+                    .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
+                
+                VStack {
+                    Text("\(vm.quote.quote)\"")
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(.black.opacity(0.5))
+                        .clipShape(.rect(cornerRadius: 25))
+                        .padding(.horizontal)
+                }
+                .frame(width: geo.size.width)
             }
+            .frame(width: geo.size.width, height: geo.size.height) // this is to make the centre of image visible for better understanding go to live mode and uncomment this line
         }
+        .ignoresSafeArea()
     }
 }
 
